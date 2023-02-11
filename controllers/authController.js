9d29801ff883c6bf5a716f60c37bfdaa73aa1cc8
Satyanarayana Devi <connect2satyam@gmail.com>
2023-02-11 13:40:14 +0530

    auto deploy enabled

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,102 +1,107 @@
-import User from '../model/User.js';
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
+import User from "../model/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const handleLogin = async (req, res) => {
-    try {
-        const cookies = req.cookies;
+  try {
+    const cookies = req.cookies;
 
-        const { email, password } = req.body;
-        if (!email || !password) return res.status(400).json({ 'message': 'Username and password are required.' });
-    
-        const foundUser = await User.findOne({ email: email }).exec();
+    const { email, password } = req.body;
+    if (!email || !password)
+      return res
+        .status(400)
+        .json({ message: "Username and password are required." });
 
-        if (!foundUser) return res.sendStatus(401); //Unauthorized 
+    const foundUser = await User.findOne({ email: email }).exec();
 
-        if (!foundUser.isActivated) return res.status(401).json({ 'message': 'User must be activated.' });
-    
-        // evaluate password 
-        const match = await bcrypt.compare(password, foundUser.password);
-        if (match) {
-    
-            // create JWTs
-            const accessToken = jwt.sign(
-                {
-                    "UserInfo": {
-                        "email": foundUser.email,
-                        "roles": foundUser.roles
-                    }
-                },
-                process.env.ACCESS_TOKEN_SECRET,
-                { expiresIn: '1m' }
-            );
-    
-            const newRefreshToken = jwt.sign(
-                { "email": foundUser.email },
-                process.env.REFRESH_TOKEN_SECRET
-            );
-    
-            // Changed to let keyword
-            let newRefreshTokenArray = !cookies?.jwt
-              ? foundUser.refreshToken
-              : foundUser.refreshToken.filter(
-                  (rtoken) => rtoken !== cookies.jwt
-                );
-    
-            if (cookies?.jwt) {
-    
-                /* 
+    if (!foundUser) return res.sendStatus(401); //Unauthorized
+
+    if (!foundUser.isActivated)
+      return res.status(401).json({ message: "User must be activated." });
+
+    const match = await bcrypt.compare(password, foundUser.password);
+    if (match) {
+      // create JWTs
+      const accessToken = jwt.sign(
+        {
+          UserInfo: {
+            email: foundUser.email,
+            roles: foundUser.roles,
+          },
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1m" }
+      );
+
+      const newRefreshToken = jwt.sign(
+        { email: foundUser.email },
+        process.env.REFRESH_TOKEN_SECRET
+      );
+
+      // Changed to let keyword
+      let newRefreshTokenArray = !cookies?.jwt
+        ? foundUser.refreshToken
+        : foundUser.refreshToken.filter((rtoken) => rtoken !== cookies.jwt);
+
+      if (cookies?.jwt) {
+        /* 
                 Scenario added here: 
                     1) User logs in but never uses RT and does not logout 
                     2) RT is stolen
                     3) If 1 & 2, reuse detection is needed to clear all RTs when email logs in
                 */
-                const refreshToken = cookies.jwt;
-                const foundToken = await User.findOne({ refreshToken }).exec();
-    
-                // Detected refresh token reuse!
-                if (!foundToken) {
-                    // clear out ALL previous refresh tokens
-                    newRefreshTokenArray = [];
-                }
-    
-                res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
-               // res.clearCookie('jwt', { httpOnly: true, sameSite: 'None'});
-            }
-    
-            // Saving refreshToken with current email
-            foundUser.refreshToken = [...newRefreshTokenArray, newRefreshToken];
-            const result = await foundUser.save();
-    
-            // Creates Secure Cookie with refresh token
-            res.cookie('jwt', newRefreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
-           // res.cookie('jwt', newRefreshToken, { httpOnly: true,sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
-    
-            // Send authorization roles and access token to email
+        const refreshToken = cookies.jwt;
+        const foundToken = await User.findOne({ refreshToken }).exec();
 
-            const currentStatus = {
-                accessToken: accessToken,
-                message: "Tokens generated successfully!",
-                status: true,
-                user: {
-                  email: foundUser.email,
-                  name: foundUser.firstName + " " + foundUser.lastName,
-                  roles: foundUser.roles
-                },
-              };
-        
-              res.status(200).send(currentStatus);
-
-
-          //  res.json({ accessToken });
-    
-        } else {
-            res.status(404).json({ message: "Invalid Password" }); // 404 Not Found
+        // Detected refresh token reuse!
+        if (!foundToken) {
+          // clear out ALL previous refresh tokens
+          newRefreshTokenArray = [];
         }
-    } catch (error) {
-        res.status(500).send("Error " + error);
+
+        res.clearCookie("jwt", {
+          httpOnly: true,
+          sameSite: "None",
+          secure: true,
+        });
+        // res.clearCookie('jwt', { httpOnly: true, sameSite: 'None'});
+      }
+
+      // Saving refreshToken with current email
+      foundUser.refreshToken = [...newRefreshTokenArray, newRefreshToken];
+      const result = await foundUser.save();
+
+      // Creates Secure Cookie with refresh token
+      res.cookie("jwt", newRefreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "None",
+        maxAge: 24 * 60 * 60 * 1000,
+      });
+      // res.cookie('jwt', newRefreshToken, { httpOnly: true,sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
+
+      // Send authorization roles and access token to email
+
+      const currentStatus = {
+        accessToken: accessToken,
+        message: "Tokens generated successfully!",
+        status: true,
+        user: {
+          email: foundUser.email,
+          name: foundUser.firstName + " " + foundUser.lastName,
+          roles: foundUser.roles,
+        },
+      };
+
+      res.status(200).send(currentStatus);
+
+      //  res.json({ accessToken });
+    } else {
+      res.status(404).json({ message: "Invalid Password" }); // 404 Not Found
     }
-   
-}
+  } catch (error) {
+    res.status(500).send("Error " + error);
+  }
+};
 
 export { handleLogin };
