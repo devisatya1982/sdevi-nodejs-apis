36e720fc34a9f062d279d88af6212cdd1571891e
Satyanarayana Devi <connect2satyam@gmail.com>
2023-02-13 20:55:42 +0530

    cookie expiration increased

--- controllers/authController.js
+++ controllers/authController.js
@@ -30,7 +30,7 @@ const handleLogin = async (req, res) => {
           },
         },
         process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: "5s" }
+        { expiresIn: "1m" }
       );
 
       const newRefreshToken = jwt.sign(
